1355af05d90b46af7983a0930687c88f786d50f8
Rodrigo Elias <rodrigo.elias@cbd.int>
2024-07-15 11:49:48 -0400

    adding tailwindcss and shadcn

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
+
 </script>
 
 <template>
-  <h1>NFP</h1>
+  <main>
+    <RouterView />
+  </main>
 </template>
---
+++ src/views/MainView.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+export default {
+    name: 'MainView',
+    components: {}
+};
+</script>
+
+<template>
+    <div>
+        <h1>NFP</h1>
+    </div>
+</template>
